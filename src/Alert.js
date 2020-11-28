be61a3787d95dcbb7106727de5e948b525a4fbf2
Armando García Paniagua <62965639+ArmandGP07@users.noreply.github.com>
2020-11-28 03:33:56 -0600

    star-wars-app

--- src/Alert.js
+++ src/Alert.js
@@ -1,10 +1,16 @@
 import './App.css';
 import React from 'react'
+
+/**
+ * Armando Garía Paniagua
+ * Joel Gómez Castro
+ */
+
 export default class Alert extends React.Component {
- render() {
- return (
-<div>{this.props.count > 60 ? "Peligro! la cantidad de segundos ha excedido el l�mte!!!" :
-""}</div>
- );
- }
-}
+    render() {
+        return (
+            <div>{this.props.count > 60 ? "Peligro! la cantidad de segundos ha excedido el límte!!!" :
+                ""}</div>
+        );
+    }
+}
